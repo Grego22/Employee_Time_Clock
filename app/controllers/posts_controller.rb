5792be6de70de1b6fca7b5991761eec3d304978d
Greg Garville Jr <gjgarvil@gmail.com>
2017-12-04 18:21:24 -0500

    created showpage and added post.inspect placeholder, rspec working clean

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,16 @@ class PostsController < ApplicationController
         @post = Post.new
     end
 
+    def create
+        @post = Post.new(params.require(:post).permit(:date, :rationale))
+
+        @post.save
+
+        redirect_to @post
+    end 
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
 end
