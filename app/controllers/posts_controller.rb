2b583311655847cc7a5d924914721c6e41133a6b
Greg Garville Jr <gjgarvil@gmail.com>
2017-12-06 16:41:22 -0500

    refactored posts controller, ran rspec to 0 failures

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,15 +7,23 @@ class PostsController < ApplicationController
     end
 
     def create
-      @post = Post.new(params.require(:post).permit(:date, :rationale))
+      @post = Post.new(post_params)
 
-      @post.save
-
-      redirect_to @post
+      if @post.save
+        redirect_to @post, notice: 'Your post was created sucessfully'
+      else 
+        render :new
+      end 
     end
 
     def show
       @post = Post.find(params[:id])
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:date, :rationale)
     end
 
 end
