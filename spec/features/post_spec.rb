4bfc6d2d48d204f3e7d13f13d0651ab0b3736a26
Greg Garville Jr <gjgarvil@gmail.com>
2017-12-07 16:20:01 -0500

    added instance variable to post spec, ran rspec

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe 'navigate' do
   before do 
     user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-     login_as(user, :scope => :user)
+     login_as(@user, :scope => :user)
   end
-  
+
   describe 'index' do
   	it 'can be reached successfully' do
   		visit posts_path
@@ -18,8 +18,8 @@ describe 'navigate' do
     end
     
     it 'has a list of posts' do
-      post1 = Post.create(date: Date.today, rationale: "Post1")
-      post2 = Post.create(date: Date.today, rationale: "Post2")
+      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
       visit posts_path
   		expect(page).to have_content(/Post1|Post2/)
     end
