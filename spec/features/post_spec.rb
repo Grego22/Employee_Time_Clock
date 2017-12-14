f74983eec164325458d6c185f736675c6bdb9f31
Greg Garville Jr <gjgarvil@gmail.com>
2017-12-14 10:20:12 -0500

    adding new and delete tests to post_spec

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -25,6 +25,18 @@ describe 'navigate' do
     end
   end
 
+    describe 'new' do
+      it 'has a link from the homepage' do
+        visit root_path 
+      
+        click_link("new_post_from_nav")
+        expect(page.status_code).to eq(200)
+      end
+    end
+
+    describe 'delete' do
+      it 'can be deleted'  do
+
   describe 'creation' do
   	before do
   		visit new_post_path
