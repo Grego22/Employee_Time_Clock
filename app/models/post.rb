018d1b5b5443eb636b3b524dfd595d5c05161fcc
Greg Garville Jr <gjgarvil@gmail.com>
2017-12-21 15:37:33 -0500

    implemented enum for status into posts for approval workflow

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+  enum status: {submitted: 0, approved: 1, rejected: 2}
+  #bypasses a lot of manual code
   validates_presence_of :date, :rationale
   belongs_to :user, optional: true
 end
+
