e364d23979c24b35e205d7a6ac4930ddc311917d
Greg Garville Jr <gjgarvil@gmail.com>
2018-01-03 11:13:03 -0500

    refactored rspec edit tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,49 +1,51 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  before do 
-    user = FactoryBot.create(:user)
-     login_as(@user, :scope => :user)
+  before do
+    @user = FactoryGirl.create(:user)
+    login_as(@user, :scope => :user)
   end
 
   describe 'index' do
+    before do
+      visit posts_path
+    end
+
   	it 'can be reached successfully' do
-  		visit posts_path
   		expect(page.status_code).to eq(200)
   	end
 
   	it 'has a title of Posts' do
-  		visit posts_path
   		expect(page).to have_content(/Posts/)
-    end
-    
+  	end
+
     it 'has a list of posts' do
-      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
-      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
+      post1 = FactoryGirl.build_stubbed(:post)
+      post2 = FactoryGirl.build_stubbed(:second_post)
       visit posts_path
-  		expect(page).to have_content(/Post1|Post2/)
+      expect(page).to have_content(/Rationale|content/)
     end
   end
 
-    describe 'new' do
-      it 'has a link from the homepage' do
-        visit root_path 
-      
-        click_link("new_post_from_nav")
-        expect(page.status_code).to eq(200)
-      end
-    end
+  describe 'new' do
+    it 'has a link from the homepage' do
+      visit root_path
 
-    describe 'delete' do
-      it 'can be deleted'  do
-      @post= FactoryBot.create(:post)      
-        visit posts_path     
-
-        click_link("delete_post_#{@post.id}_from_nav")
-        expect(page.status_code).to eq(200)
-      end
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
     end
-    
+  end
+
+  describe 'delete' do
+    it 'can be deleted' do
+      @post = FactoryGirl.create(:post)
+      visit posts_path
+
+      click_link("delete_post_#{@post.id}_from_index")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
   describe 'creation' do
   	before do
   		visit new_post_path
@@ -70,40 +72,31 @@ describe 'navigate' do
     end
   end
 
-   describe 'edit' do
+  describe 'edit' do
     before do
-      @post= FactoryBot.create(:post)
-    end
-    it 'can be reached by clicking edit on index page' do
-      visit posts_path
-
-      click_link("edit_#{post.id})")
-      #changed from click_on edit to click_link
-      expect(page.status_code).to eq(200)
+      @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      login_as(@edit_user, :scope => :user)
+      @edit_post = Post.create(date: Date.today, rationale: "asdf", user_id: @edit_user.id)
     end
 
     it 'can be edited' do
-      test_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf" )
-      visit edit_post_path(@post)
-      test_post = Post.create(date: Date.today, rationale: "asdf")
-
-      logout(:edit)
-      login_as(@non_authorized_user, :scope => :user)
+      visit edit_post_path(@edit_post)
 
       fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "Edited Content"
+      fill_in 'post[rationale]', with: "Edited content"
       click_on "Save"
-      
-      expect(page).to have_content("Edited Content")
 
-      it 'cannot be edited by a non authorized user' do
-        logout(:edit)
-        @non_authorized_user = FactoryBot.create(:non_authorized_user)
-        login_as(@non_authorized_user, :scope => :user)
+      expect(page).to have_content("Edited content")
+    end
 
-        visit edit_post_path
+    it 'cannot be edited by a non authorized user' do
+      logout(:user)
+      non_authorized_user = FactoryGirl.create(:non_authorized_user)
+      login_as(non_authorized_user, :scope => :user)
 
-        expect(current_page).to eq(root_path)
+      visit edit_post_path(@edit_post)
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
