65251afad1044d1fc71506be7fa648ee9defc73f
Greg Garville Jr <gjgarvil@gmail.com>
2018-01-03 15:07:50 -0500

    added third user in posts.rb, author: non_authorized_user

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,13 +1,20 @@
 FactoryBot.define do
-    factory :post do
-        date Date.today
-        rationale "Some Rationale"
-        user
-    end
+  factory :post do
+    date Date.today
+    rationale "Some Rationale"
+    user
+  end
 
-    factory :second_post, class: "Post" do
-        date Date.yesterday
-        rationale "Some More Content"
-        user
-    end
+  factory :second_post, class: "Post" do
+    date Date.yesterday
+    rationale "Some More Content"
+    user
+  end
+
+  factory :post_from_other_user, class: "Post" do
+    date Date.yesterday
+    rationale "Some More Content"
+    non_authorized_user
+  end
+
 end
