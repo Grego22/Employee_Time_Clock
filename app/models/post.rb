8b46b97fad8574bb0d11a5678fab62d35f21c88c
Greg Garville Jr <gjgarvil@gmail.com>
2018-01-05 00:38:38 -0500

    added overtime_request to post model, seeded db, and tested on local server

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   enum status: {submitted: 0, approved: 1, rejected: 2}
   #bypasses a lot of manual code
-  validates_presence_of :date, :rationale
+  validates_presence_of :date, :rationale, :overtime_request
   belongs_to :user
 
   scope :posts_by, ->(user) {where(user_id: user.id)}
