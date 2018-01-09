8397c388b8efd37557315123a300c8b8a6ebb723
Greg Garville Jr <gjgarvil@gmail.com>
2018-01-09 12:02:17 -0500

    required phone attribute to user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,7 +9,7 @@ FactoryBot.define do
     email { generate :email }
     password "asdfasdf"
     password_confirmation "asdfasdf"
-    phone: "4322386131"
+    phone "4322386131"
   end
 
   factory :admin_user, class: "AdminUser" do
@@ -18,7 +18,7 @@ FactoryBot.define do
     email { generate :email }
     password "asdfasdf"
     password_confirmation "asdfasdf"
-    phone: "4322386131"
+    phone "4322386131"
   end
 
   factory :non_authorized_user, class: "User" do
@@ -27,6 +27,6 @@ FactoryBot.define do
     email { generate :email }
     password "asdfasdf"
     password_confirmation "asdfasdf"
-    phone: "4322386131"
+    phone "4322386131"
   end
 end
