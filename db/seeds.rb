74e1d7028aedb69c71d8f57732a7de456d4a0fd0
Greg Garville Jr <gjgarvil@gmail.com>
2018-01-10 13:08:06 -0500

    Updated audit log factory and tested out 6 day default

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,4 +22,10 @@ puts "1 admin User Created Boss!"
                  overtime_request: 2.5)
 end
 
-puts "100 posts have been created"
+100.times do |audit_log|
+    AuditLog.create!(user_id: User.last.id,
+                 status: 0,
+                 start_date: (Date.today - 6.days))
+end
+
+puts "100 auditlogs  have been created"
