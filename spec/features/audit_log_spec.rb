5dcb1dbd92a4a924e911370e864ebaef74278e16
Greg Garville Jr <gjgarvil@gmail.com>
2018-01-11 17:22:36 -0500

    updated routes, added index method in audit_logs controller, and added index

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 describe 'AuditLog Feature' do
-  let {audit_log = FactoryBotRails.create(:audit_log)}
+  let (:audit_log){FactoryBotRails.create(:audit_log)}
 
   describe 'index' do
     it 'has an index page that can be reached'
       visit audit_logs_path
       expect(page.status_code)
 
+
+    it "renders audit log content" do
+      visit audit_logs_path
+      expect(page).to have_content
+    end
   end
 end
