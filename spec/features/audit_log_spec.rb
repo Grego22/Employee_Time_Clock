46bc86de981d6b10d67ac447c386d08380bd7d9c
Greg Garville Jr <gjgarvil@gmail.com>
2018-01-11 18:27:18 -0500

    implemented initial view code for audit logs and continued working on audit_log validations

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -1,17 +1,26 @@
 require 'rails_helper'
 
 describe 'AuditLog Feature' do
-  let (:audit_log){FactoryBotRails.create(:audit_log)}
-
   describe 'index' do
+    before do
+      admin_user = FactoryBotRails.create(:admin_user)
+      login_as(admin_user, :scope => admin_user)
+      FactoryBotRails.create(:admin_user)
+    end
+
     it 'has an index page that can be reached'
       visit audit_logs_path
       expect(page.status_code)
 
 
     it "renders audit log content" do
+      FactoryBotRails.create(:audit_log)
       visit audit_logs_path
-      expect(page).to have_content
+      expect(page).to have_content(/Snow/)
+    end
+
+    xit 'cannot be accessed by non admin users' do
+
     end
   end
 end
