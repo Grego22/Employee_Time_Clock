c2d36d5f18fc46f658b2215b94129ab9923faab5
Greg Garville Jr <gjgarvil@gmail.com>
2018-01-11 20:03:58 -0500

    blocked non admin users from accessing index page for audits

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -1,5 +1,7 @@
 class AuditLogsController < ApplicationController
   def index
     @audit_logs = AuditLog.all
+    authorize @audit_logs
+    #important to have this order like so, authorize second
   end
 end
