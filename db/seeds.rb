fc360d1d2474100ea689bcfa5821a5127917e2f7
Greg Garville Jr <gjgarvil@gmail.com>
2018-01-26 16:16:06 -0500

    built out rake task for sending sms messages and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-@user = User.create(email: "test@test.com",
+@employee = Employee.create(email: "test@test.com",
                     password: "asdfasdf",
                     password_confirmation: "asdfasdf",
                     first_name: "Jon",
                     last_name: "Snow",
                     phone: "4322386131")
-puts "1 user created"
+puts "1 employee created"
 
 AdminUser.create(email: "admin@test.com",
                  password: "asdfasdf",
@@ -12,17 +12,17 @@ AdminUser.create(email: "admin@test.com",
                  first_name: "Admin",
                  last_name: "Hux",
                  phone: "4322386131")
-puts "1 admin User Created Boss!"
+puts "1 admin employee Created Boss!"
 
 
 
-AuditLog.create!(user_id: @user.id,
+AuditLog.create!(user_id: @employee.id,
                  status: 0,
                  start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id,
+AuditLog.create!(user_id: @employee.id,
                  status: 0,
                  start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id,
+AuditLog.create!(user_id: @employee.id,
                  status: 0,
                  start_date: (Date.today - 20.days))
 
@@ -36,7 +36,7 @@ consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolor
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-                 user_id: @user.id,
+                 user_id: @employee.id,
                  overtime_request: 2.5)
 end
 
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -1,16 +1,14 @@
 namespace :notification do
   desc "Sends SMS notification to employees asking to log overtime, (if necessary)."
   task sms: :environment do
-    # 1. Schedule to run at Sunday at 5PM
-    # 2. Iterate over all emoployees
-    # 3. Skip AdminUsers
-    # 4. Send a message that has instructions and a link to log time
-    # User.all.each do |user|
-    #   SmsTool.send_sms()
-    # No spaces or dashes in phone number
-    # exactly 10 charactewrs
-    # all characters have to be a number
-    #
+   if Time.now.sunday?
+     employees = Employee.all
+     notifcation_message = "Please log into the overtime management dashboard to request overtime or confirm your hours for last week: #{request.domain}"
+
+     employees.each do |employee|
+       SmsTool.send_sms(number: employee.phone, message: notifcation_message)
+     end
+   end
   end
 
   desc "Sends mail notifications to managers (admin users) each day to inform of pending overtime requests"
