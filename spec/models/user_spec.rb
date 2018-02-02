6025bbfa28dc96ff9badac5905dae99ca1677d4c
Greg Garville Jr <gjgarvil@gmail.com>
2018-02-02 13:17:59 -0500

    added relationships in user and hand models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,6 +52,10 @@ RSpec.describe User, type: :model do
 		it "allows for admins to be associated with multiple employees" do
 			employee_1 = FactoryBotRails.create(:user)
 			employee_2 = FactoryBotRails.create(:user)
+      admin = FactoryBotRails.create(:admin_user)
+			Hand.create!(user_id: admin.id, hand_id: employee_1.id)
+			Hand.create!(user_id: admin.id, hand_id: employee_2.id)
+      expect(admin.hands.count). eq(2)
 
 		end
 	end
